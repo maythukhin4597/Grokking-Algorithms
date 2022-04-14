e821cd8623aa18b25289d18a1aac5e12c4f6f74a
maythukhin <maythukhin.uit@gmail.com>
2022-04-14 15:19:32 +0630

    Add Factorial and Binary search with recursion

---
+++ grokkingalgo/src/chapter3/FactorialRecursion.java
@@ -0,0 +1,16 @@
+package chapter3;
+
+public class FactorialRecursion {
+
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println(factorial(num));
+    }
+
+    private static int factorial(int num) {
+        if (num == 1) {
+            return 1;
+        }
+        return num * factorial(num - 1);
+    }
+}
